5d2952e6a350110387c822a96da4ee58b8e64906
Joshua Donahue <jtdonahue360@gmail.com>
2019-05-18 14:10:55 -0400

    initial commit; somewhat working calculator

---
+++ javascript/index.js
@@ -0,0 +1,46 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    display: "",
+    register: 0,
+    operation: ""
+  },
+
+  methods: {
+    number: function(number){
+      if(this.display.length < 8){
+        this.display += number;
+      }
+    },
+
+    operate: function(operation) {
+      this.register += parseInt(this.display, 10);
+      this.display = "";
+      this.operation = operation;
+    },
+
+    equals: function() {
+      switch(this.operation) {
+        case "+":
+          this.display = this.register + parseInt(this.display, 10);
+        break;
+        case "-":
+          this.display = this.register - parseInt(this.display, 10);
+        break;
+        case "*":
+          this.display = this.register * parseInt(this.display, 10);
+        break;        
+        case "/":
+          this.display = this.register / parseInt(this.display, 10);
+        break;
+        default:
+          alert("default case reached");
+        break;
+      }
+      this.register = 0;
+      this.operation = "";
+    }
+
+  }
+})
+
